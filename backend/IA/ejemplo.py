9a51b91bf0fd1599c60582b4a319b4a762d07ea0
Jenry560 <115893645+Jenry560@users.noreply.github.com>
2025-12-06 20:00:09 -0400

    Refactor code structure for improved readability and maintainability

--- backend/IA/ejemplo.py
+++ backend/IA/ejemplo.py
@@ -1,6 +1,12 @@
 from RealEstateSmartPredictor import RealEstateSmartPredictor
+import json
+import pandas as pd
 
 predictor = RealEstateSmartPredictor()
 
 
 resultados = predictor.predict()
+
+# Guardar resultados en CSV
+df = pd.DataFrame(resultados)
+df.to_csv("resultados_prediccion.csv", index=False)
